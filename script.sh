1d2b0fa490a5706d1c82f6afeb96bebd876f72f5
minhtanle <39462813+minhtanle@users.noreply.github.com>
2019-12-20 15:07:15 +0700

    Script backup

--- script.sh
+++ script.sh
@@ -5,8 +5,8 @@
 BACKUP_DIR=/usr/src/
 DAYS_TO_KEEP=5
 FILE_SUFFIX=_psql_backup.sql
-DATABASE=fusionpbx
-USER=user_connect
+DATABASE=db2019
+USER=user2019
 PASSWORD=password1234567
 
 FILE=`date +"%Y%m%d_%H%M%S"`_${DATABASE}${FILE_SUFFIX}
@@ -19,7 +19,3 @@ PGPASSWORD=${PASSWORD} pg_dump -Fc -U ${USER} ${DATABASE} -f ${OUTPUT_FILE}
 
 # remove old backups
 find $BACKUP_DIR -maxdepth 1 -mtime +$DAYS_TO_KEEP -name "*${FILE_SUFFIX}" -exec rm -rf '{}' ';'
-
-# HOW TO RESTORE
-# pg_restore --clean -Fc -U fusionpbx -d fusionpbx < 20191220_231023_fusionpbx_psql_backup.sql
-# Password:
